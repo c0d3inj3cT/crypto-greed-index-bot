b212a62d305b1c690fb6dc2c47278ab317c9b913
root <root@server1.zerotrustprotection.com>
2021-08-08 20:19:29 +0000

    implemented redis cache

--- bot.py
+++ bot.py
@@ -5,10 +5,13 @@ import requests
 import json
 import binascii
 from dotenv import load_dotenv
+import redis
 
 '''
 A simple Discord bot which will return the current crypto greed and fear index
 Commands can be invoked directly from the Discord server
+
+Response from the API will be cached in a redis cache for 1 hour
 '''
 
 load_dotenv()
@@ -16,7 +19,7 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 emojis = [u'\U0001F631', u'\U0001F61F', u'\U0001F610', u'\U0001F60B', u'\U0001F924']
-
+r = redis.Redis(host='localhost', port=6379, db=0)
 url = "https://api.alternative.me/fng/?limit=2"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11.2; rv:88.0) Gecko/20100101 Firefox/88.0'}
 
@@ -26,28 +29,37 @@ bot = commands.Bot(command_prefix="!")
 # to use it, enter the command as !index
 @bot.command(name='index')
 async def on_message(ctx):
-    res = requests.get(url, headers=headers)
-    response = res.text
+    if r.get('value') == None:
+        res = requests.get(url, headers=headers)
+        response = res.text
 
-    d = json.loads(response)
+        d = json.loads(response)
 
-    value = d["data"][0]["value_classification"]
-    score = d["data"][0]["value"]
+        value = d["data"][0]["value_classification"]
+        score = d["data"][0]["value"]
 
-    if value.lower() == "extreme fear":
-        emoji = emojis[0]
-    elif value.lower() == "fear":
-        emoji = emojis[1]
-    elif value.lower() == "neutral":
-        emoji = emojis[2]
-    elif value.lower() == "greed":
-        print("we are here")
-        emoji = emojis[3]
-    elif value.lower() == "extreme greed":
-        emoji = emojis[4]
+        r.set('value', value)
+        r.expire('value', 3600)
+        r.set('score', score)
+        r.expire('score', 3600)
+
+        if value.lower() == "extreme fear":
+            emoji = emojis[0]
+        elif value.lower() == "fear":
+            emoji = emojis[1]
+        elif value.lower() == "neutral":
+            emoji = emojis[2]
+        elif value.lower() == "greed":
+            print("we are here")
+            emoji = emojis[3]
+        elif value.lower() == "extreme greed":
+            emoji = emojis[4]
+    else:
+        value = r.get('value')
+        score = r.get('score')
 
     msg = "Crypto greed and fear index rating is: " + value + " right now " + emoji + "\n" + "score: " + score
 
     await ctx.send(msg)
-    
+
 bot.run(TOKEN)
